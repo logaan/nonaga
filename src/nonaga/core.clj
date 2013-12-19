8ff4c9c86f345c20f5fad39f3993a49694234929
Logan Campbell <colin@logaan.net>
2013-12-19 22:41:19 +1100

    move fn and tests in test folder

--- src/nonaga/core.clj
+++ src/nonaga/core.clj
@@ -1,10 +1,9 @@
-(ns nonaga.core
-  (:use [clojure.test]))
+(ns nonaga.core)
 
 (def initial-game
   {:rings
     #{   [1 4] [2 4] [3 4]  
-       [0 3] [1 3] [2 3] [4 3]
+       [0 3] [1 3] [2 3] [3 3]
      [0 2] [1 2] [2 2] [3 2] [4 2] 
        [0 1] [1 1] [2 1] [3 1] 
          [1 0] [2 0] [3 0]}
@@ -30,29 +29,14 @@
 (defn sw [[x y]] (if (odd?  y) [x (- y 1)] [(- x 1) (- y 1)]))
 (defn w  [[x y]]                           [(- x 1) y      ])
 
-(deftest directional-functions
-  (are [function start expected]
-       (= expected (take 5 (iterate function start)))
-
-       nw [3 0] [[3 0] [2 1] [2 2] [1 3] [1 4]]
-       ne [1 0] [[1 0] [1 1] [2 2] [2 3] [3 4]]
-       e  [0 2] [[0 2] [1 2] [2 2] [3 2] [4 2]]
-       se [1 4] [[1 4] [1 3] [2 2] [2 1] [3 0]]
-       sw [3 4] [[3 4] [2 3] [2 2] [1 1] [1 0]]
-       w  [4 2] [[4 2] [3 2] [2 2] [1 2] [0 2]]))
-
 (defn invalid-space? [{:keys [rings whites blacks]} coord]
   (not (and (rings coord)
             (not (or (whites coord)
                      (blacks coord))))))
 
-(deftest invalid-space
-  (are [coord expected]
-       (= expected (invalid-space? initial-game coord))
-       
-       [0 4] true
-       [1 4] true
-       [2 4] false))
-
-(run-tests)
+(defn move [board coord direction]
+  (first
+    (for [step (iterate direction coord)
+          :when (invalid-space? board (direction step))]
+      step)))
 
--- test/nonaga/core_test.clj
+++ test/nonaga/core_test.clj
@@ -2,6 +2,31 @@
   (:require [clojure.test :refer :all]
             [nonaga.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest directional-functions
+  (are [function start expected]
+       (= expected (take 5 (iterate function start)))
+
+       nw [3 0] [[3 0] [2 1] [2 2] [1 3] [1 4]]
+       ne [1 0] [[1 0] [1 1] [2 2] [2 3] [3 4]]
+       e  [0 2] [[0 2] [1 2] [2 2] [3 2] [4 2]]
+       se [1 4] [[1 4] [1 3] [2 2] [2 1] [3 0]]
+       sw [3 4] [[3 4] [2 3] [2 2] [1 1] [1 0]]
+       w  [4 2] [[4 2] [3 2] [2 2] [1 2] [0 2]]))
+
+(deftest invalid-space
+  (are [coord expected]
+       (= expected (invalid-space? initial-game coord))
+       
+       [0 4] true
+       [1 4] true
+       [2 4] false))
+
+(deftest movement
+  (are [coord direction expected]
+       (= expected (move initial-game coord direction))
+
+       [3 0] nw [1 3]
+       [0 3] e  [3 3]
+       [0 1] e  [3 1]))
+
+(run-tests)
