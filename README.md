a747a6742d6fcd8bc79bfd7e3fd1c0fc5d8039ab
Logan Campbell <colin@logaan.net>
2013-12-19 23:49:00 +1100

    updated readme and project with desc

--- README.md
+++ README.md
@@ -1,14 +1,12 @@
 # nonaga
 
-A Clojure library designed to ... well, that part is up to you.
-
-## Usage
-
-FIXME
+A Clojure implementation of the game Spanish table top game [Nonaga] [bgg].
 
 ## License
 
-Copyright © 2013 FIXME
+Copyright © 2013 Logan Campbell
 
 Distributed under the Eclipse Public License either version 1.0 or (at
 your option) any later version.
+
+[bgg]: http://boardgamegeek.com/boardgame/46614/nonaga
--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject nonaga "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A Clojure implementation of the game Spanish table top game
+               Nonaga."
+  :url "https://github.com/logaan/nonaga"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
