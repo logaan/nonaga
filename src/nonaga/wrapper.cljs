0f103af1eb0510a84450fff34ecd0375678180df
Logan Campbell <colin@logaan.net>
2014-01-02 15:11:39 +1100

    making better use of js/ interop

--- src/nonaga/wrapper.cljs
+++ src/nonaga/wrapper.cljs
@@ -1,14 +1,14 @@
 (ns nonaga.wrapper)
 
 (defn svg [props child]
-  (.svg (.-DOM js/React) (clj->js props) (clj->js child)))
+  (js/React.DOM.svg (clj->js props) (clj->js child)))
 
 (defn circle [props]
-  (.circle (.-DOM js/React) (clj->js props)))
+  (js/React.DOM.circle (clj->js props)))
 
 (defn create-class [& {:as config}]
-  (.createClass js/React (clj->js config)))
+  (js/React.createClass (clj->js config)))
 
 (defn render-component [component node]
-  (.renderComponent js/React component node))
+  (js/React.renderComponent component node))
 
