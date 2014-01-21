e51486cfa8708a4066accedb31aa888d6da992de
Logan Campbell <colin@logaan.net>
2014-01-22 00:00:36 +1100

    draw-marbles to multi method

--- src/nonaga/draw/draw_marbles.cljs
+++ src/nonaga/draw/draw_marbles.cljs
@@ -1,18 +1,24 @@
 (ns nonaga.draw.draw-marbles
-  (:use [nonaga.draw.util :only [hex->svg marble update-state]]))
+  (:use [nonaga.draw.util :only [hex->svg marble update-state event-type]]))
 
 (defn start-marble-move [component color coord]
   (update-state component #(assoc % :event [:marble-selected color coord])))
 
-(defn draw-marbles [component state color]
-  (let [etype  (get-in state [:event 0])
-        cp     (get-in state [:event 1])
-        coords (color state)
+(derive :turn-began      ::marble-selectable)
+(derive :marble-selected ::marble-selectable)
+
+(defmulti draw-marbles (fn [_ state _] (event-type state)))
+
+(defmethod draw-marbles :default [component state color]
+  (let [marbles (color state)]
+    (map (partial marble color) (map hex->svg marbles))))
+
+; Color and cp are badly named. Should be color-of-marbles-being-drawn and
+; current player.
+(defmethod draw-marbles ::marble-selectable
+  [component {[t cp] :event :as state} color]
+  (let [coords (color state)
         click  (partial start-marble-move component)]
-    (map (fn [hex svg]
-           (marble color (if (and (or (= etype :turn-began)
-                                      (= etype :marble-selected))
-                                  (= cp color))
-                           (click color hex)) svg))
+    (map (fn [hex svg] (marble color (if (= cp color) (click color hex)) svg))
          coords (map hex->svg coords))))
 
