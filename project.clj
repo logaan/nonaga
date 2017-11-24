681bd556cc3ca4a9486578b564916e8e0741631b
Tom Parker <palfrey@tevp.net>
2017-11-24 20:19:45 +0000

    Fix the build with modern Clojurescript

--- project.clj
+++ project.clj
@@ -4,14 +4,15 @@
   :url "https://github.com/logaan/nonaga"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "6969"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                [org.clojure/clojurescript "1.9.946"]
                  [com.cemerick/clojurescript.test "0.2.1"]
                  [com.cemerick/piggieback "0.1.2"]]
-  :plugins [[lein-cljsbuild "1.0.1"]
+  :plugins [[lein-cljsbuild "1.1.7"]
             [com.cemerick/clojurescript.test "0.2.1"]]
   :externs ["externs/react.js"]
   :hooks  [leiningen.cljsbuild]
+  :pedantic? true
   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
   :cljsbuild {:builds {:dev {:source-paths ["src"]
                              :compiler {:output-to "resources/js/main.js"
